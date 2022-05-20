9fffd456eb4ec87380d76a827caa7243c2aa7371
Eva Reitzel <evareitzel@gmail.com>
2022-05-20 13:42:07 -0400

    commit tree c2d6e5bdb62ae39bacc17a18aad6cd32a2cfef5f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(num){
+    if (num > 42) {
+        return num - 42;
+    }
+    return 42 - num;
+}
+ 
+function distanceFromHqInFeet (num2){
+    return (distanceFromHqInBlocks(num2)) * 264;
+}
+ 
+function distanceTravelledInFeet(start, destination){
+    if (start > destination) {
+        return (start - destination) * 264;
+    }
+    else {
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    // if (distanceTravelledInFeet(start, destination) <= 400) {
+    if (distance <= 400) {
+        return 0;
+    } else if ((distance > 400) && (distance <= 2000)) {
+        return (distance-400) * .02;
+    } else if ((distance > 2000) && (distance <= 2500)) {
+        return 25;
+    } else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+}
